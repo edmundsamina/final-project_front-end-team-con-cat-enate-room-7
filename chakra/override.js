dd554a14d5236a2034921bf4c8ad86e5d0cfbe57
St0neofFr33dom <christophefreeston@googlemail.com>
2022-07-26 15:11:03 +0100

    Changed overides key values to numbers, works now

--- chakra/override.js
+++ chakra/override.js
@@ -3,8 +3,9 @@ import { extendTheme } from "@chakra-ui/react";
 const overrides = {
   colors: {
     roots: {
-       " mainColor": "#448FFF",
-       "secondColor":"#B2D1FF",
+       100: "#448FFF",
+       50:"#B2D1FF",
+       0:"#FFFFFF"
     },
   },
 };

0fb10cdd11f5757a650e85803f865bf6a49b6c62
St0neofFr33dom <christophefreeston@googlemail.com>
2022-07-26 13:49:21 +0100

    Initalised branch for the AddButton component

---
+++ Components/addButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {AddIcon} from '@chakra-ui/icons'
+import {IconButton} from '@chakra-ui/react'
+
+const AddButton = () => {
+    return (
+        <span>
+
+        </span>
+    )
+}
+
+export default AddButton

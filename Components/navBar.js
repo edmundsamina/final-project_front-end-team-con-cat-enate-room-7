0cf5f0b7fae876d96892c338a2e2d96d2577e5b8
HW Chong <103215075+LunaChong@users.noreply.github.com>
2022-07-26 12:23:52 +0100

    add classname and resize logo
    add classname and resize logo

--- Components/navBar.js
+++ Components/navBar.js
@@ -4,8 +4,8 @@ import Image from "next/image.js";
 
 export default function NavBar() {
   return (
-    <div>
-      <Image src="/../public/conCAT.png" alt="Care-Full Logo" width={100} height={100} />
+    <div className="navbar">
+      <Image src="/../public/conCAT.png" alt="Care-Full Logo" width={30} height={30} />
       <h2>are-Full</h2>
       <Hamburger />
     </div>

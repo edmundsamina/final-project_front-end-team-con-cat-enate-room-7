43c4968cad11bb6e0acf2f9062c6a43db471744c
St0neofFr33dom <christophefreeston@googlemail.com>
2022-07-26 10:43:54 +0100

    Replaced image holder with the custom conCat.png found in the public folder

--- Components/navBar.js
+++ Components/navBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hamburger from "./hamburger.js";
+import Image from "next/image.js";
 
 export default function NavBar() {
   return (
     <div>
-      <p>Image placeholder</p>
+      <Image src="/../public/conCAT.png" alt="Care-Full Logo" width={100} height={100} />
+      <h2>are-Full</h2>
       <Hamburger />
     </div>
   );

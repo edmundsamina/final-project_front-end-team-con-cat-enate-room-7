68fe99729d64f03e2a393fdd98c007f98a75b10b
St0neofFr33dom <christophefreeston@googlemail.com>
2022-07-28 15:49:36 +0100

    Started skeleton of form

--- pages/addSymptom.js
+++ pages/addSymptom.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+  } from '@chakra-ui/react'
+import NavBar from '../Components/navBar'
+import LinkButton from '../Components/linkButton'
 
-const componentName = () => {
+const AddSymptom = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <NavBar />
+            <FormControl>
+            <FormLabel>Add Symptom</FormLabel>
+                <Input placeholder='Symptom' />
+                <Input placeholder='Date' />
+                <Input placeholder='Time' />
+                <Input placeholder='Description' />
+            </FormControl>
+            <LinkButton text="Add" link="/symptoms"/>
         </div>
     )
 }
 
-export default componentName
+export default AddSymptom

43ee4587b6b0bb03d6c63db427f6d494617616d1
St0neofFr33dom <christophefreeston@googlemail.com>
2022-07-28 14:01:16 +0100

    Returning dummy values for all pages

---
+++ pages/addReminder.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddReminder = () => {
+    return (
+        <div>
+            Coming Soon
+        </div>
+    )
+}
+
+export default AddReminder
---
+++ pages/pets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pets = () => {
+    return (
+        <div>
+            Coming Soon 
+        </div>
+    )
+}
+
+export default Pets

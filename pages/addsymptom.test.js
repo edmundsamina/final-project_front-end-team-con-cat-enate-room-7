cd3d2f398a01a75a108ff6dbe9c13c339afa09e1
JackC91 <jackjcherry@hotmail.co.uk>
2022-07-29 11:53:41 +0100

    basic render unit tests done for addsymptom and adincident forms

---
+++ pages/addsymptom.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import AddSymptom from "./addSymptom.js";
+import "@testing-library/jest-dom";
+import "@jest/globals"
+
+describe("Add Symptom page", () => {
+    render(<AddSymptom/>)
+it("checks that the add symptom input form renders correctly on the page", () => {
+expect(screen.getByPlaceholderText("Description")).toBeInTheDocument();
+})
+})

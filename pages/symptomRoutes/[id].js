6ecde5ef30f08550da6d65f9197d32d9e1dba615
Miguel Lamas <lamasmigs@yahoo.co.uk>
2022-07-29 14:33:24 +0100

    getStaticPaths to recognise ID

--- pages/symptomRoutes/[id].js
+++ pages/symptomRoutes/[id].js
@@ -1,6 +1,36 @@
 import React from "react";
 
-function Details() {
+const url = process.env.NEXT_PUBLIC_DB_URL ?? "http://localhost:3000";
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`${url}/symptoms`);
+  console.log(res)
+  const data = await res.json();
+console.log(data);
+  const paths = data.payload.map(symptoms => {
+      return {
+  params: { id: symptoms.symptoms_id.toString() }
+}
+})
+
+return {
+  paths,
+  fallback: false
+}
+}
+
+// export const getStaticProps = async (context) => {
+//   const id = context.params.id;
+//   const res = await fetch(`${url}/symptoms/` + id);
+//   const data = await res.json();
+
+//   return {
+//     props: {incidents: data}
+//   }
+// }
+
+
+const Details = () => {
   return (
     <div>
       <h1>Details</h1>

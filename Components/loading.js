f6b06346f350ddaf2a14bd900cabb6c3e17650c8
lunaM <potterwo123@gmail.com>
2022-07-29 09:32:41 +0100

    add loading

---
+++ Components/loading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className='loadcat'>
+          <div className="cat">
+          <div className="cat__body"></div>
+          <div className="cat__body"></div>
+          <div className="cat__tail"></div>
+          <div className="cat__head"></div>
+          </div>
+        </div>
+    )
+}
+
+export default Loading
--- pages/pets.js
+++ pages/pets.js
@@ -2,15 +2,17 @@ import React from "react";
 import NavBar from "../Components/navBar.js";
 import Image from "next/image.js";
 import PetButton from "../Components/petButton"
+import Loading from "../Components/loading.js";
 
 const SchedulePage = () => {
  
   return (
     <main>
       <NavBar />
-      <Image src={require("./../public/conCAT.png")} alt="Care-Full image" width={30} height={30} />
+      <Image src={require("./../public/mock_photo.jpg")} alt="Care-Full image" />
       <div className="m10">
           <PetButton text="Change Pet Info" link=""/>
+          <Loading />
       </div>
     </main>
   );

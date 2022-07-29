d8fef2711c89931bdac03b69c30b75e5fdea3f55
St0neofFr33dom <christophefreeston@googlemail.com>
2022-07-29 10:56:08 +0100

    Created a functioning form for the Add Symptom Page, uses nanoid to produce incident and symptom ids, form can't submit until all fields are filled, need to refactor the user and pet id as state, once we have can craete and fetch it

--- pages/addSymptom.js
+++ pages/addSymptom.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     FormControl,
     FormLabel,
@@ -8,14 +8,45 @@ import {
   } from '@chakra-ui/react'
 import NavBar from '../Components/navBar'
 import LinkButton from '../Components/linkButton'
+import { nanoid } from 'nanoid/non-secure'
 
 const url = process.env.NEXT_PUBLIC_DB_URL
 const AddSymptom = () => {
 
-    async function handlePost(body){
+    const user_id = "1234567890"
+    const pet_id = "1234567890"// These ids need to be handed in as state/context
+
+    const [submission,setSubmission] = useState({
+        user_id: user_id,
+        pet_id: pet_id,
+        incident_id: nanoid(10),
+        symptoms: "",
+        symptoms_id: nanoid(10),
+        date: "",
+        time: "",
+        description: ""
+    })
+
+    const [noEmptyFields, setNoEmptyFields] = useState(false)
+
+     function handleChange(e){
+        let value = (e.target.value).toString()
+        setSubmission({ ...submission, [e.target.name]: value });
+        let entries = Object.values(submission);
+        for (let i = 0; i < entries.length; i++) {
+          if (entries[i] === "" || entries[i] === undefined) {
+            setNoEmptyFields(false)
+            return
+          } 
+        
+        }
+        setNoEmptyFields(true)
+     }
+
+    async function handlePost(){
         const response = await fetch(`${url}/symptoms`, {
             method: "POST",
-            body: JSON.stringify(body),
+            body: JSON.stringify(submission),
             headers: {
                 'Content-Type': 'application/json'
               }
@@ -29,12 +60,12 @@ const AddSymptom = () => {
             <NavBar />
             <FormControl>
             <FormLabel>Add Symptom</FormLabel>
-                <Input placeholder='Symptom' />
-                <Input placeholder='Date' />
-                <Input placeholder='Time' />
-                <Input placeholder='Description' />
+                <Input placeholder='Symptom' name="symptoms" value={submission.symptoms} onChange={handleChange}/>
+                <Input placeholder='Date' type="date" name="date" value={submission.date} onChange={handleChange}/>
+                <Input placeholder='Time' type="time" name="time" value={submission.time} onChange={handleChange}/>
+                <Input placeholder='Description' name="description" value={submission.description} onChange={handleChange}/>
             </FormControl>
-            <LinkButton text="Add" link="/symptoms"/>
+            {noEmptyFields && <LinkButton text="Add" link="/symptoms" onClick={handlePost}/>}
         </div>
     )
 }

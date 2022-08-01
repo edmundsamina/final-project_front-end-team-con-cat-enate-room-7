d7aff24ca4ee97779cb6b3b3363611961eaba28f
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-08-01 09:42:59 +0100

    error seems to have been fixed - build command

--- Tests/addsymptom.test.js
+++ Tests/addsymptom.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import AddSymptom from "./addSymptom.js";
+import AddSymptom from "../pages/addSymptom.js";
 import "@testing-library/jest-dom";
 import "@jest/globals"
 

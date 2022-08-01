42967d7b583d49802e91249f45b0c5d07123e597
CodeNameMoe <newmoeali@gmail.com>
2022-08-01 12:34:14 +0100

    created pet card component and added main image to home page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
-import AddButton from '../Components/addButton'
-import NavBar from '../Components/navBar'
-import styles from '../styles/Home.module.css'
-import SymptomCard from '../Components/symptomCard'
-import LinkButton from '../Components/linkButton'
+import AddButton from "../Components/addButton";
+import NavBar from "../Components/navBar";
+import styles from "../styles/Home.module.css";
+import SymptomCard from "../Components/symptomCard";
+import LinkButton from "../Components/linkButton";
+import Image from "next/image";
+import PetCard from "../Components/PetCard";
 
 export default function Home() {
-
-  function onClick (){
-    console.log("I am here!")
+  function onClick() {
+    console.log("I am here!");
   }
   return (
-    <div className={styles.container}>
+    <main>
       <NavBar />
-    </div>
-  )
+      <div className={styles.container}>
+        <Image
+          className="home-image"
+          src={require("./../public/mock_photo.jpg")}
+          alt="Picture of cat and dog"
+          layout="responsive"
+        />
+        <PetCard name="Fluffy" image="pet-card-cat.png" />
+      </div>
+    </main>
+  );
 }

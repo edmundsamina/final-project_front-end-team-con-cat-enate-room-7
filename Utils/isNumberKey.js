dcc0a5372a107c010fb363a191519a8341627b2f
St0neofFr33dom <christophefreeston@googlemail.com>
2022-08-04 14:44:39 +0100

    Added patterns to the inputs and validators in the functions, will have to test on iphone to see if it works. Also added a maximum chcracter length of 16 on name and breed, maximum of 2 characters on age, and kg can input anywhere between 0 and 100, with up to 2 decimeal places

---
+++ Utils/isNumberKey.js
@@ -0,0 +1,6 @@
+export default function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}

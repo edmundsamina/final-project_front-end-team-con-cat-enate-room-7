95a2d05d275c634b5a9221ac05a96001113e2468
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-05 12:19:19 +0100

    further text alterations

--- Components/petNavBar.js
+++ Components/petNavBar.js
@@ -8,7 +8,7 @@ import InfoModal from "./modal.js";
 export default function PetNavBar({pet}) {
   return (
     <div className="navbar">
-    <InfoModal title="Pet Page- Info" text="Welcome to your Pet Page, here you can start adding your pets that you would like to tack using this app. To begin press the Add Button at the bottom of the screen and fill in some information about your pet. You will be re-directed back to this page and will see a card with your pet's name on it. Press that card to continue."/>
+    <InfoModal title="Pet Page- Info" text="Welcome to your Pet Page, here you can start adding your pets that you would like to tack using this app. To begin press the Add Button (+) at the bottom of the screen and fill in some information about your pet. You will be re-directed back to this page and will see a card with your pet's name on it. Press that card to continue."/>
       <IndexPage text="Care-Full"/>
       {/* <Image src={require("./../public/conCAT.png")} alt="Care-Full Logo" width={30} height={30} />
       <h2>are-Full</h2> */}

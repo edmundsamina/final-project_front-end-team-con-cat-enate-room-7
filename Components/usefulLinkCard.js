fb11117e3e0e337e33b423ba44b9f9fb86d21657
unknown <jackjcherry@hotmail.co.uk>
2022-08-08 11:04:09 +0100

    ironing out array import quirks

--- Components/usefulLinkCard.js
+++ Components/usefulLinkCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Image from "next/image.js";
 
-const UsefulLinkCard = ({src, alt, text, title, link}) => {
+const UsefulLinkCard = ({src, alt, text, title, href}) => {
     return (
-        <a href={link}>
+        <a href={href}>
         <div className='link-card'>
             <h2>{title}</h2>
-            {/* <Image src={src} alt={alt}/> */}
+            <Image src={src} alt={alt}/>
             <p>{text}</p>
         </div>
         </a>
--- pages/[pets]/usefulLinks.js
+++ pages/[pets]/usefulLinks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from '../../Components/navBar.js';
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import UsefulLinkCard from '../../Components/usefulLinkCard.js';
+import catUsefulLinks from '../../libs/catUsefulLinks.js';
 
 const url = process.env.NEXT_PUBLIC_DB_URL ?? "http://localhost:3000"
 
@@ -13,11 +14,18 @@ const response = await fetch(`${url}/pets?pet_id=${id}`)
  return {props:{pet:data.payload[0]}}
   }
 
-export default withPageAuthRequired (function UsefulLinks({pet}) {
+export default withPageAuthRequired (function UsefulLinks({pet},catUsefulLinks) {
+  console.log(catUsefulLinks)
   return (
         <main className='links-page'>
             <NavBar pet={pet}/>
-            <UsefulLinkCard link='' title='Test' href='' alt='' text='Testing'/>
+            <div>
+            {catUsefulLinks.map((link) => {
+              return (
+            <UsefulLinkCard key={link.title} href={link.link} title={link.title} src={link.image} alt='' text={link.text}/>
+            );
+        })}
+            </div>
         </main>
     )
 }

3ff3ea8beb374a019606977ae9515903717d33cc
unknown <jackjcherry@hotmail.co.uk>
2022-08-08 16:36:50 +0100

    attempting to mock auth0 in addincident test suite

--- Tests/addincident.test.js
+++ Tests/addincident.test.js
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import AddIncident from "../pages/addIncident.js";
+import AddIncident from "../pages/[pets]/symptoms/[id]/addIncident.js";
 import "@testing-library/jest-dom";
-import "@jest/globals"
+import "@jest/globals";
+import { UserProvider } from '@auth0/nextjs-auth0';
+
+jest.mock('@auth0/nextjs-auth0', () => ({ UserProvider: ({ children }) => '<div>{children}</div>' }));
 
 describe("Add Incident page", () => {
-    render(<AddIncident/>)
+    render(
+    <UserProvider>
+    <AddIncident incidents={"props"}/>
+    </UserProvider>)
 it("checks that the add incident input form renders correctly on the page", () => {
 expect(screen.getByPlaceholderText("Description")).toBeInTheDocument();
 })

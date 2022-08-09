2712d83c7624aa5189b7bafa7a882a7ddc06db96
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-08-09 11:13:46 +0100

    changed gif to GIF in 404

--- pages/404.js
+++ pages/404.js
@@ -10,7 +10,7 @@ const pageNotFound = () => {
 					<Image
 						className="home-image"
 						unoptimized={true}
-						src={require("./../public/test.gif")}
+						src={require("./../public/test.GIF")}
 						alt="Picture of cat and dog"
 					/>
 					<h1>404</h1>

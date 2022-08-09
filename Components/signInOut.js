89a58ac1608ef7d8043ef7cf121d641ed2bd7210
lunaM <potterwo123@gmail.com>
2022-08-09 14:51:56 +0100

    new image in sign in out component and add useMediaQuery to shift 2 image

--- Components/signInOut.js
+++ Components/signInOut.js
@@ -2,17 +2,65 @@ import React from 'react'
 import Image from 'next/image'
 import LinkButton from './linkButton'
 
+import { useState, useCallback, useEffect } from 'react';
+
+const useMediaQuery = (width) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addListener(updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeListener(updateTarget);
+  }, []);
+
+  return targetReached;
+};
+
+
 const SignInOut = () => {
+    const isBreakpoint = useMediaQuery(450)
+
     return (
-        <div className='not-fund'>
+        <div className='sign-in'>
          <div>
-          <Image
+            <p>Welcome to</p>
+            <h2>The Care-Full App</h2>
+            <div>
+                { isBreakpoint ? (
+                    <Image
+                     src={require("/public/big_line.png")}
+                     alt="Picture of cat and dog"/>
+                ) : (
+                    <Image
+                     src={require("/public/line_black.png")}
+                     alt="Picture of cat and dog"/>
+                    )
+                }
+              </div>
+            <h2>Helping owners care for their pets</h2>
+            <p>since 2022</p>
+        </div>
+          {/* <Image
               className="home-image"
               src={require("./../public/conCAT.png")}
               alt="Picture of cat and dog"
               />
          </div>
-        <h2>Welcome to The Care-Full App</h2>
+        <h2>Welcome to The Care-Full App</h2> */}
         <LinkButton text="Log In" link="/api/auth/login"/>
         </div>
     )

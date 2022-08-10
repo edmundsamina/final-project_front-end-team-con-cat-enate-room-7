3dd3f106719e5548093d7ac733bfb97bb959345d
unknown <jackjcherry@hotmail.co.uk>
2022-08-10 16:02:25 +0100

    symptom id index page cleaned up of old references to getstaticpaths and props

--- pages/[pets]/symptoms/[id]/index.js
+++ pages/[pets]/symptoms/[id]/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import LinkButton from '../../../../Components/linkButton'
 import SymptomDetailsCard from '../../../../Components/symptomDetailsCard'
 import NavBar from "../../../../Components/navBar.js";
-import { useUser } from "@auth0/nextjs-auth0";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 
@@ -20,7 +19,7 @@ const response = await fetch(`${url}/symptoms/${pet_id}?symptoms_id=${symptoms_i
 
 
 export default withPageAuthRequired (function Details({incidents}) {
-  const { user, error, isLoading } = useUser();
+
   return (
 		<main>
 			<NavBar pet={incidents[0]}/>
@@ -44,41 +43,3 @@ export default withPageAuthRequired (function Details({incidents}) {
 	);
 }
 )
-
-
-// export const getStaticPaths = async () => {
-//   const petId = "1234567890" //To be handed in as a prop
-//   const res = await fetch(`${url}/symptoms/${petId}`);
-//   const data = await res.json();
-//   function removeNulls(data){
-//     let newArray = [];
-//     for(let i = 0; i<data.length; i++){
-//       let valuesArray = Object.values(data[i]);
-//       if (!valuesArray.includes(null) && !valuesArray.includes(undefined)){
-//         newArray.push(data[i])
-//       }
-//     }
-//     return newArray
-//   }
-//   let validData = removeNulls(data.payload)
-//   const paths = validData.map(symptoms => {
-//       return {
-//   params: { id: symptoms.symptoms_id.toString() }
-// }
-// })
-
-// return {
-//   paths,
-//   fallback: false
-// }
-// }
-
-// export const getStaticProps = async (context) => {
-//   const symptomsId = context.params.id;
-//   const petId = "1234567890" //To be handed in as a prop
-//   const res = await fetch(`${url}/symptoms/${petId}?symptoms_id=${symptomsId}`);
-//   const data = await res.json();
-//   return {
-//     props: {incidents: data.payload}
-//   }
-// }

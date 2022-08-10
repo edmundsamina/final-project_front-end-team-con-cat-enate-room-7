93c8021690cc32f212386bfeee0a5503f7199ab8
unknown <jackjcherry@hotmail.co.uk>
2022-08-10 16:11:14 +0100

    schedule index cleaned up

--- pages/[pets]/schedule/index.js
+++ pages/[pets]/schedule/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import NavBar from "../../../Components/navBar.js";
 import AddButton from "../../../Components/addButton.js";
 import ScheduleCard from "../../../Components/scheduleCard";
-import CompletedTaskCard from "../../../Components/completedTaskCard.js";
 import { useEffect, useState } from "react";
 import { nanoid } from "nanoid/non-secure";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
@@ -29,7 +28,7 @@ export default withPageAuthRequired (function SchedulePage({pet}) {
 		resolve => setTimeout(resolve, ms)
 	  );
 	useEffect(() => {
-		// declare the data fetching function
+
 		const fetchData = async () => {
 			await delay(500)
 			const response = await fetch(`${url}/reminders?pet_id=${pet.pet_id}`);
@@ -38,14 +37,11 @@ export default withPageAuthRequired (function SchedulePage({pet}) {
 			setData(newArray);
 		};
 
-		// call the function
-		fetchData()
-			// make sure to catch any error
-			.catch(console.error);
+		fetchData().catch(console.error);
 	}, []);
 
 	useEffect(() => {
-		// declare the data fetching function
+
 		const fetchData = async () => {
 			const response = await fetch(`${url}/reminders?pet_id=${pet.pet_id}`);
 			const data = await response.json();
@@ -53,19 +49,13 @@ export default withPageAuthRequired (function SchedulePage({pet}) {
 			setData(newArray);
 		};
 
-		// call the function
-		fetchData()
-			// make sure to catch any error
-			.catch(console.error);
+		fetchData().catch(console.error);
 	}, [stateCount]);
 
 	if (!data) {
 		return <Loader/>;
 	}
 
-	/*
-  need to add a post method to this function to create a copy of the card going into completed, but generate a new reminder_id, and perform an update on the date based on frequency chosen at inital creation
-  */
 	async function patchFunction(data) {
 		await fetch(`${url}/reminders/${data.reminder_id}`, {
 			method: "PATCH",
@@ -148,7 +138,6 @@ export default withPageAuthRequired (function SchedulePage({pet}) {
 	return (
 		<main>
 			<NavBar pet={pet}/>
-			{/* <h3>{pet.pet_id}</h3> */}
 			<div className="m10">
 			<InfoModal title="Schedule Page- Info" text="Welcome to your pet's Schedule Page. Here you can keep track of anything your pet needs during care. Start by pressing the Add Button (+) and filling out the form, after this you will be re-directed back to this page. Anytime you have done the reminder you have added simply press Done where the date will update to the next time you need to repeat this task and a log of what you have already done will be kept in the View History section."/>
 				<h2 className="text-center">Check Schedule</h2>
@@ -169,19 +158,3 @@ export default withPageAuthRequired (function SchedulePage({pet}) {
 	);
 }
 )
-
-	// if (data.length === 0) {
-	// 	return (
-	// 		<main>
-	// 		<NavBar pet={pet}/>
-	// 		{/* <h3>{pet.pet_id}</h3> */}
-	// 		<div className="m10">
-	// 		<InfoModal title="Schedule Page- Info" text="Welcome to your pet's Schedule Page. Here you can keep track of anything your pet needs during care. Start by pressing the Add Button (+) and filling out the form, after this you will be re-directed back to this page. Anytime you have done the reminder you have added simply press Done where the date will update to the next time you need to repeat this task and a log of what you have already done will be kept in the View History section"/>
-	// 			<h2 className="text-center">Check Schedule</h2>
-	// 		      <h2>{pet.name}</h2>
-	// 					<NoDataCard text="You haven't added any reminders yet. Click the Add Button below to get started" />
-	// 		</div>
-	// 		<AddButton text="Add Reminder" href={{pathname:`schedule/addReminder`, query:{pets:`${pet.pet_id}`}}} />
-	// 	</main>
-	// 	)
-	// }
